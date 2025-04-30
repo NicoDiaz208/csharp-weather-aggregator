8888a4db55b0fa0572961f4b53966c8e41888638
NicoDiaz208 <nicodiaz208@gmail.com>
2025-04-30 09:26:18 +0200

    recon gitignore

--- WeatherAggregator/WeatherAggregator.Library/Entities/WeatherInfo.cs
+++ WeatherAggregator/WeatherAggregator.Library/Entities/WeatherInfo.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WeatherAggregator.Library.Interfaces;
 
 namespace WeatherAggregator.Library.Entities
 {
-    public class WeatherInfo
+    public class WeatherInfo: IWeatherInfo
     {
         public double Temperature { get; set; }
         public string Time { get; set; }
